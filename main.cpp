2be3c21f80519dad82e61413c10d9e0667ae73a1
AchroDev <achrodev@proton.me>
2024-10-06 16:59:04 -0500

    Scopes

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 // Object (Stack/Scope) Lifetimes in C++
 //
 
+class Entity
+{
+    // Class Scope
+private:
+    int x;
+};
+
 int main()
 {
+    // Function scope
+
+    if (true)
+    {
+        // If Scope
+    }
+
+    {
+        // Empty Scope
+    }
 
     std::cin.get();
 }
