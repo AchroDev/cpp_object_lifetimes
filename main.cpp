80d9938580a164fc76adc4197182c38793d10b77
AchroDev <achrodev@proton.me>
2024-10-06 17:20:38 -0500

    Added example ScopedPointer class to show how it works and calls the ScopedPointer on the heap in the entry point to example it will be destroyed outside the scope

--- main.cpp
+++ main.cpp
@@ -17,13 +17,37 @@ public:
     }
 };
 
+// In the case you want to instantiate an object on the heap and have it automatically delted once out of scope,
+// you can achieve this by using a unique_ptr (aka. Unique/Scoped Pointer). While the std library includes this,
+// for this example, we will write our own
+
+class ScopedPtr
+{
+    // "Hard coded to only accept Entities"
+private:
+    Entity *m_Ptr;
+
+public:
+    ScopedPtr(Entity *ptr)
+        : m_Ptr(ptr)
+    {
+    }
+
+    ~ScopedPtr()
+    {
+        delete m_Ptr;
+    }
+};
+
+// End basic scoped pointer example code
+
 int main()
 {
 
     {
         // Empty Scope
-        Entity e;                  // Calling on the stack and will be deleted once outside the scope
-        Entity *e2 = new Entity(); // Calling on the heap and won't be deleted until called or when the program exits
+        ScopedPtr e = new Entity(); // Instead of instantiating the heap Entity like before, we can use the ScopedPtr to make it automatically get destroyed once
+                                    // outside the scope
     }
 
     std::cin.get();
