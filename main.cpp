251929ca636d36b40909fc3405612eab615c3ade
AchroDev <achrodev@proton.me>
2024-10-06 17:06:03 -0500

    Switched to public and created a constructor and destructor in the Entity class. In the empty scope in the entry point, I instantiated two instaces of Entity. e which lives on the stack and e2 which lives on the heap

--- main.cpp
+++ main.cpp
@@ -5,22 +5,25 @@
 
 class Entity
 {
-    // Class Scope
-private:
-    int x;
+public:
+    Entity()
+    {
+        std::cout << "Created Entity" << std::endl;
+    }
+
+    ~Entity()
+    {
+        std::cout << "Destroyed Entity" << std::endl;
+    }
 };
 
 int main()
 {
-    // Function scope
-
-    if (true)
-    {
-        // If Scope
-    }
 
     {
         // Empty Scope
+        Entity e;                  // Calling on the stack and will be deleted once outside the scope
+        Entity *e2 = new Entity(); // Calling on the heap and won't be deleted until called or when the program exits
     }
 
     std::cin.get();
